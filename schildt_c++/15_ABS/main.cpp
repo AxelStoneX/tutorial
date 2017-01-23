eef7e73da27de342094958ff836fb87258d6a53f
Alex Stone <kongol@live.ru>
2017-01-23 21:16:58 +0300

    schildt_c++: old tutorials had been deleted

--- schildt_c++/15_ABS/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cstdlib>
-using namespace std;
-
-int main(void)
-{
-   cout << abs(-10) << "\n";
-   return 0;
-}
--- schildt_c++/6_CONTROL_SEQUENCE/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(void)
-{
-    cout << "\n\\\b\a\f\a\a\a\f\\";
-    return 0;
-}
